9aacec9faac5442e90f0c7d5919fc5de51091dcf
pranav259 <72136233+pranav259@users.noreply.github.com>
2025-07-10 16:44:21 +0530

    Add files via upload

---
+++ editor.lua
@@ -0,0 +1,9 @@
+return {
+  -- Hihglight colors
+  {
+    "echasnovski/mini.hipatterns",
+    event = "BufReadPre",
+    opts = {},
+  },
+  -- You dont need to set any of these options. These are the default ones. Only
+}
